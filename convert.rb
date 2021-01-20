3c89351746c1c392753be3b2c5d2cc1e95f5693a
AstonGemmy <astongemmy@gmail.com>
2021-01-20 12:14:27 +0100

    Added Comments for easy reading

--- convert.rb
+++ convert.rb
@@ -1,6 +1,13 @@
+# Requiring/Importing activeSupport gem for parsing xml to ruby hash
 require 'active_support/core_ext/hash'
+# Requiring/importing json gem for parsing ruby hash to json formatted data
 require 'json'
+
+# Reading xml file 'hr_test_1.xml' from root directory
 xml = File.open('hr_test_1.xml').read
+# Parsing read xml file to Ruby hashes
 json = Hash.from_xml(xml).to_json
-newJson = JSON.pretty_generate(JSON.parse(Hash.from_xml(xml).to_json))
-File.open('hr_test_1.json', 'w+').write newJson
+# Applying pretty print style to json formatted data
+prettyJson = JSON.pretty_generate(JSON.parse(json))
+# Writing json formatted data as json file in root directory with name 'hr_test_.json'
+File.open('hr_test_1.json', 'w+').write prettyJson
--- test/controllers/file_parser_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class FileParserControllerTest < ActionDispatch::IntegrationTest
-  test "should get convert" do
-    get file_parser_convert_url
-    assert_response :success
-  end
-end
